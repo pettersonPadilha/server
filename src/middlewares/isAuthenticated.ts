07000fea5f1cdd0df12e7225dac43fd861a4901f
Petter Padilha <petterson_sorriso@hotmail.com>
2023-02-12 20:27:29 -0400

    dados autenticados

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -19,6 +19,9 @@ export function isAuthenticated(req:Request, res:Response, next:NextFunction) {
     try {
         //validar o token
         const {sub} = verify(token,process.env.JWT_SECRET) as payLoad;
+
+        //Recuperar o id do token e colocar dentro de uma variavel user_id dentro do request
+        req.user_id = sub
         return next();
         
     }catch(err) {
