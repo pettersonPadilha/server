a4a4410495fdc4c99eb0210bf59952d0f3929ed1
Petter Padilha <petterson_sorriso@hotmail.com>
2023-03-05 00:36:09 -0400

    Melhoria no codigo

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -11,20 +11,22 @@ export function isAuthenticated(req:Request, res:Response, next:NextFunction) {
     //Receber um token 
     const authToken = req.headers.authorization;
     if(!authToken) {
-        return res.status(401).end();
+        return res.status(404).send({message: "Token invalid"});
     }
 
     const [, token] = authToken.split(" ");
 
     try {
         //validar o token
-        const {sub} = verify(token,process.env.JWT_SECRET) as payLoad;
+        const decoded = verify(token,process.env.JWT_SECRET)
+        const {sub} = decoded as payLoad;
+        // const {sub} = verify(token,process.env.JWT_SECRET) as payLoad;
 
         //Recuperar o id do token e colocar dentro de uma variavel user_id dentro do request
         req.user_id = sub
         return next();
         
     }catch(err) {
-        return res.status(401).end();
+        return res.status(401).send({message: "token not valid / inspired"});
     }
 }
