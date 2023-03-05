29b993bdecb642b493e8e23accee95806c42e072
Petter Padilha <petterson_sorriso@hotmail.com>
2023-03-05 00:56:03 -0400

    Criando a rota da listagem

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import {AuthUserController} from "./controllers/user/AuthUserController";
 import {DetailUserController} from "./controllers/user/DetailUserController";
 import {isAuthenticated} from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 const router = Router();
 
@@ -19,5 +20,6 @@ router.get("/me",      isAuthenticated, new DetailUserController().handle)
 **/
 
 router.post("/category",isAuthenticated, new CreateCategoryController().handle)
+router.get("/categoryall", isAuthenticated, new ListCategoryController().handle)
 
 export {router};
